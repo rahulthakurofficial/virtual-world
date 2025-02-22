d9bd080703c5e2d1c42e982ee1623acad98038e3
Rahul Kumar Thakur <rahulthakur.official6@gmail.com>
2025-02-22 19:37:20 +0530

    Fixed GitHub Actions workflow for deployment

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,17 @@
-import { defineConfig } from 'vite'
+// import { defineConfig } from 'vite'
 
-import react from '@vitejs/plugin-react'
+// import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
+
+// export default defineConfig({
+//   plugins: [react()],
+// })
+
+
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
 export default defineConfig({
   plugins: [react()],
-})
+  base: "/virtual-world/", 
+});
